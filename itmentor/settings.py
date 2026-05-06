a04a9670be3ec9e9ea95b9a69e129961d3c33473
DonDon696 <dbankov06@gmail.com>
2026-05-06 19:34:22 +0300

    update

--- itmentor/settings.py
+++ itmentor/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('localhost'),
+        'HOST': os.getenv('POSTGRES_HOST'),
         'PORT': os.getenv('POSTGRES_PORT'),
     }
 }
@@ -165,3 +165,4 @@ LOGGING = {
         },
     },
 }
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
